4b8cf2f70535891151c17da96a38b9122ad18ee3
QuyenNNM0723 <144427889+quyen0723@users.noreply.github.com>
2025-12-24 20:28:35 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+// ===========================================
+// CẤU HÌNH ẢNH - THAY LINK ẢNH CỦA BẠN Ở ĐÂY
+// ===========================================
+
+const IMAGE_URLS = [
+    'https://i.ibb.co/7xnYQWVX/christmas-wreath.png',
+    'https://i.ibb.co/Swr5DSvq/gingerbread.png',
+    'https://i.ibb.co/Mx3WD0Kg/santa-claus.png',
+    'https://i.ibb.co/Fb8CtGzg/christmas-tree.png',
+    'https://i.ibb.co/JwYsPvKQ/christmas-wreath-1.png',
+    'https://i.ibb.co/pjPHmrXd/presents.png',
+    'https://i.ibb.co/KzNTfj9S/santa-hat.png',
+    'https://i.ibb.co/f6mPHJh/christmas-card.png',
+    'https://i.ibb.co/C5GW1F4M/stick-candy.png',
+];
+
+export default IMAGE_URLS;
